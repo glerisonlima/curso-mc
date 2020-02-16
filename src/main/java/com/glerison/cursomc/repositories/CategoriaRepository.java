8a95776d585427f7aebe00dac887e8762f834818
glerison lima <glerionlima@gmail.com>
2020-02-16 00:11:03 -0300

    criando repository e service para categoria

---
+++ src/main/java/com/glerison/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.glerison.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.glerison.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
--- src/main/java/com/glerison/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/glerison/cursomc/resources/CategoriaResources.java
@@ -3,26 +3,26 @@ package com.glerison.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.glerison.cursomc.domain.Categoria;
+import com.glerison.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@GetMapping
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(1, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
