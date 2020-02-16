711d76b5b49847584a2991fec3b822c14078f70e
glerison lima <glerionlima@gmail.com>
2020-02-15 22:54:55 -0300

    Testando primeira classe de dominio

---
+++ src/main/java/com/glerison/cursomc/resources/CategoriaResources.java
@@ -0,0 +1,28 @@
+package com.glerison.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.glerison.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResources {
+
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(1, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+}
