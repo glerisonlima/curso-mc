8f688176d303f1ef57f9a056a5e5eb276f66e7d0
glerison lima <glerionlima@gmail.com>
2020-02-18 01:01:28 -0300

    ajuste do endpoint /categoria/{id}

--- src/main/java/com/glerison/cursomc/domain/Produto.java
+++ src/main/java/com/glerison/cursomc/domain/Produto.java
@@ -12,6 +12,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Produto implements Serializable{
 	
@@ -23,11 +26,13 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", 
 				joinColumns = @JoinColumn(name = "produto_id"),
 				inverseJoinColumns = @JoinColumn(name = "categoria_id") 
 	)
+	@JsonBackReference
 	private List<Categoria> categorias = new ArrayList<>();
 	
 	public Produto() {}
